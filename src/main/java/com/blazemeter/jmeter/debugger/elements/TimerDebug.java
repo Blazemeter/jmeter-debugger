866be42e2abae1a110f6a2ca8c30c309ee9a7d0f
Andrey Pohilko <apc4@ya.ru>
2016-05-23 16:04:45 +0300

    Don't pause on timers

--- src/main/java/com/blazemeter/jmeter/debugger/elements/TimerDebug.java
+++ src/main/java/com/blazemeter/jmeter/debugger/elements/TimerDebug.java
@@ -2,8 +2,12 @@ package com.blazemeter.jmeter.debugger.elements;
 
 import com.blazemeter.jmeter.debugger.engine.StepTrigger;
 import org.apache.jmeter.timers.Timer;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
 
 public class TimerDebug extends AbstractDebugElement<Timer> implements Timer {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
     public TimerDebug(Timer te, StepTrigger hook) {
         super(te, hook);
     }
@@ -11,6 +15,8 @@ public class TimerDebug extends AbstractDebugElement<Timer> implements Timer {
     @Override
     public long delay() {
         hook.notify(this);
-        return parent.delay();
+        long delay = parent.delay();
+        log.debug("Drop delay because of debug: " + delay);
+        return 0;
     }
 }
