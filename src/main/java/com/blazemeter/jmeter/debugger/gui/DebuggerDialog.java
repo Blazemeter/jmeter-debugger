f59adbc90477afb1a9670e8e50dbae87fbae9923
Andrey Pohilko <apc4@ya.ru>
2016-05-23 16:04:55 +0300

    clear element pane on stop

--- src/main/java/com/blazemeter/jmeter/debugger/gui/DebuggerDialog.java
+++ src/main/java/com/blazemeter/jmeter/debugger/gui/DebuggerDialog.java
@@ -113,6 +113,7 @@ public class DebuggerDialog extends DebuggerDialogBase {
             start.setEnabled(true);
             stop.setEnabled(false);
             tgCombo.setEnabled(true);
+            elementContainer.removeAll();
         }
     }
 
