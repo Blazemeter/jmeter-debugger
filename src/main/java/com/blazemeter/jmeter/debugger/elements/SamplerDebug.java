27c64bff4f4e67ebae4a18dca288b6e212e836ac
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-07-17 18:04:33 +0300

    fixed variables

--- src/main/java/com/blazemeter/jmeter/debugger/elements/SamplerDebug.java
+++ src/main/java/com/blazemeter/jmeter/debugger/elements/SamplerDebug.java
@@ -3,6 +3,7 @@ package com.blazemeter.jmeter.debugger.elements;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
+import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.apache.jmeter.threads.JMeterContext;
 
 
@@ -23,5 +24,11 @@ public class SamplerDebug extends AbstractDebugElement<Sampler> implements Sampl
     @Override
     public void setThreadName(String inthreadName) {
         wrapped.setThreadName(inthreadName);
+    }
+
+    @Override
+    public void setProperty(JMeterProperty property) {
+        super.setProperty(property);
+        wrapped.setProperty(property);
     }
 }
